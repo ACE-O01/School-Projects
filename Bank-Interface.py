8392d95b4895524d00287e00725cb6bc60242d7f
Madhav <at.sarojkumar01@gmail.com>
2025-11-20 03:35:32 +0530

    Add files via upload
    Bank Login/Sign-up Interface

---
+++ Bank-Interface.py
@@ -0,0 +1,14 @@
+print("Welcome To AIB Bank")
+
+name = input("Enter Your Name: ")
+acc_no = input("Enter The Account Number: ")
+
+while True:
+    pin = input("Enter a 4-digit PIN Number: ")
+    
+    # Check if PIN is exactly 4 digits AND contains only numbers
+    if len(pin) != 4 or not pin.isdigit():
+        print("Please enter a PIN with exactly 4 digits.")
+    else:
+        print("Your Account Has Been Successfully Created!")
+        break
